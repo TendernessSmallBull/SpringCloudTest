bd67fcc23caa512e8e39713e68c0ae3b1b579d19
zhaoxin <1026243373@qq.com>
2019-02-19 14:07:12 +0800

    修改类名

--- springcloudparent/springcloud-eureka-client/src/main/java/com/springcloud/demo/EurekaClientApplication.java
+++ springcloudparent/springcloud-eureka-client/src/main/java/com/springcloud/demo/EurekaClientApplication.java
@@ -17,9 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
-public class ServiceHiApplication {
+public class EurekaClientApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceHiApplication.class, args);
+		SpringApplication.run(EurekaClientApplication.class, args);
 	}
 
 	@Value("${server.port}")
