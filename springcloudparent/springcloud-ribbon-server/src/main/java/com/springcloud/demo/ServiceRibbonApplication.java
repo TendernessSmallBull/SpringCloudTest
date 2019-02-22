1249b7fb9ef40b3c28622af71a7515a00b0a1e33
zhaoxin <1026243373@qq.com>
2019-02-22 15:47:32 +0800

    复习重点

--- springcloudparent/springcloud-ribbon-server/src/main/java/com/springcloud/demo/ServiceRibbonApplication.java
+++ springcloudparent/springcloud-ribbon-server/src/main/java/com/springcloud/demo/ServiceRibbonApplication.java
@@ -9,14 +9,14 @@ import org.springframework.web.client.RestTemplate;
 
 /**  
  * Description: @EnableDiscoveryClient spring cloud中discovery service有许多种实现
- * （eureka、consul、zookeeper等等），@EnableDiscoveryClient基于spring-cloud-commons, 
+    *   （eureka、consul、zookeeper等等），@EnableDiscoveryClient基于spring-cloud-commons, 
  * @EnableEurekaClient基于spring-cloud-netflix。其实用更简单的话来说，就是如果选用的注册中心是eureka，
- * 那么就推荐@EnableEurekaClient，如果是其他的注册中心，那么推荐使用@EnableDiscoveryClient。
- * 注解@EnableEurekaClient上有@EnableDiscoveryClient注解，
- * 可以说基本就是EnableEurekaClient有@EnableDiscoveryClient的功能，
- * 另外上面的注释中提到，其实@EnableEurekaClientz注解就是一种方便使用eureka的注解而已，
- * 可以说使用其他的注册中心后，都可以使用@EnableDiscoveryClient注解，
- * 但是使用@EnableEurekaClient的情景，就是在服务采用eureka作为注册中心的时候，使用场景较为单一。
+    *    那么就推荐@EnableEurekaClient，如果是其他的注册中心，那么推荐使用@EnableDiscoveryClient。
+    *    注解@EnableEurekaClient上有@EnableDiscoveryClient注解，
+    *    可以说基本就是EnableEurekaClient有@EnableDiscoveryClient的功能，
+    *    另外上面的注释中提到，其实@EnableEurekaClientz注解就是一种方便使用eureka的注解而已，
+    *    可以说使用其他的注册中心后，都可以使用@EnableDiscoveryClient注解，
+    *    但是使用@EnableEurekaClient的情景，就是在服务采用eureka作为注册中心的时候，使用场景较为单一。
  * @author zx  
  * @date 2019年2月19日  
  */
