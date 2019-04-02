84c565fbe6b767b1019b43f3762350cd866cd604
zhaoxin <1026243373@qq.com>
2019-04-02 15:03:38 +0800

    添加断路器监控组件Hystrix Dashboard

--- springcloudparent/springcloud-ribbon-server/src/main/java/com/springcloud/demo/RibbonServerApplication.java
+++ springcloudparent/springcloud-ribbon-server/src/main/java/com/springcloud/demo/RibbonServerApplication.java
@@ -2,9 +2,11 @@ package com.springcloud.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -28,6 +30,8 @@ import com.springcloud.demo.config.MyLoadBalanceConfig;
 @EnableDiscoveryClient
 @RibbonClient(name="ribbonClient",configuration=MyLoadBalanceConfig.class)
 @EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class RibbonServerApplication {
 
 	public static void main(String[] args) {
